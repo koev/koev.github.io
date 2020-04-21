934ff8df535ae9747a036893cfd253faae8025c0
Krasimir Koev <krasimir.koev@techhuddle.com>
2020-04-21 14:23:44 +0300

    no message

---
+++ todo-hooks/precache-manifest.09be5284cc2976e6ea9f6ce1226565b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbd97b3f81684ccba029",
+    "url": "/static/css/main.afaf731e.chunk.css"
+  },
+  {
+    "revision": "fbd97b3f81684ccba029",
+    "url": "/static/js/main.535c0430.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c3f38d636f23bd1f51a",
+    "url": "/static/js/2.7f5ca7ab.chunk.js"
+  },
+  {
+    "revision": "9126916acbb0071cb74a1f9a5aaba489",
+    "url": "/index.html"
+  }
+];
